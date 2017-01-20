3c054bd279fee80055db6d79e7b6f9bd3a64009c
Rocke <909104374@qq.com>
2017-01-20 17:07:42 +0800

    修改了一些bug，校验了一两个暂时木有发现什么问题

--- com/tigerjoys/messagecontent/jieguohuizong.py
+++ com/tigerjoys/messagecontent/jieguohuizong.py
@@ -25,7 +25,7 @@ def noProvince():
     dbMysqlConn = MySQLdb.connect(host='192.168.12.155', user='guoliufang', passwd='tiger2108', db='honeycomb',
                                   use_unicode=True, port=5209, charset='utf8')
     myExecutor = dbMysqlConn.cursor()
-    smsSQL = """select extract(YEAR_MONTH FROM `record_time`) as yuefen,yewucode_name,count(DISTINCT uuid) from message_analysises GROUP  BY yuefen,yewucode_name"""
+    smsSQL = """select extract(YEAR_MONTH FROM `record_time`) as yuefen, yewucode_name, count(DISTINCT uuid) from message_analysises where yewucode_name is not null and yewucode_name != '-1' and status = 2 GROUP  BY yuefen,yewucode_name"""
     myExecutor.execute(smsSQL)
     smsListTuple = myExecutor.fetchall()
     dbGpsqlConn = psycopg2.connect(database='tjdw', user='tj_root', password='77pbV1YU!T', host='192.168.12.14',
@@ -64,15 +64,56 @@ def noProvince():
     csvfile.close()
 
 
-# def withProvince():
+def withProvince():
+    csvfile = open("/data/sdg/guoliufang/mysqloutfile/withProvince.txt", mode='wa+')
+    # csvfile = open("/Users/LiuFangGuo/Downloads/withProvince.txt", mode='wa+')
+    csvlist = []
+    dbMysqlConn = MySQLdb.connect(host='192.168.12.155', user='guoliufang', passwd='tiger2108', db='honeycomb',
+                                  use_unicode=True, port=5209, charset='utf8')
+    myExecutor = dbMysqlConn.cursor()
+    smsSQL = """select extract(YEAR_MONTH FROM `record_time`) as yuefen, yewucode_name, province_id, count(DISTINCT uuid) from message_analysises where yewucode_name is not null and yewucode_name != '-1' and province_id != -1 and status = 2 GROUP  BY yuefen,yewucode_name,province_id"""
+    myExecutor.execute(smsSQL)
+    smsListTuple = myExecutor.fetchall()
+    dbGpsqlConn = psycopg2.connect(database='tjdw', user='tj_root', password='77pbV1YU!T', host='192.168.12.14',
+                                   port=5432)
+    gpExecutor = dbGpsqlConn.cursor()
+    for smsTuple in smsListTuple:
+        # 0-yuefen,1-yewu,3-duanxin,2-province-id
+        start_end = getFormatStartEnd(str(smsTuple[0]))
+        unionNameSQL = """select distinct union_name from charge_codes_statistics where yewucode_name = '""" + smsTuple[
+            1] + """'"""
+        myExecutor.execute(unionNameSQL)
+        unionNameListTuple = myExecutor.fetchall()
+        inUnionNameListPartSQL = ''
+        for unionNameTuple in unionNameListTuple:
+            inUnionNameListPartSQL = """','""".join(str(e) for e in unionNameTuple) + """','""" + inUnionNameListPartSQL
+        inUnionNameListPartSQL = inUnionNameListPartSQL[:-3]
+        gpXiaFaLiLunShiJiSQL = "select code_event,count(distinct uuid) from BDL.crab_code_histories  where record_time BETWEEN " + \
+                               start_end[0] + " and " + start_end[
+                                   1] + " and code_union_name in ('" + inUnionNameListPartSQL + "') and province_id = " + str(
+            smsTuple[2]) + " group by code_event order by code_event"
+        gpExecutor.execute(gpXiaFaLiLunShiJiSQL)
+        XiaFaLiLunShiJiTupleList = gpExecutor.fetchall()
+        a10 = 0
+        a20 = 0
+        a30 = 0
+        for rTuple in XiaFaLiLunShiJiTupleList:
+            if rTuple[0] == 10:
+                a10 = rTuple[1]
+            elif rTuple[0] == 20:
+                a20 = rTuple[1]
+            elif rTuple[0] == 30:
+                a30 = rTuple[1]
+        inUnionNameListPartSQL = "'" + inUnionNameListPartSQL + "'"
+        csvlist.append((smsTuple[0], smsTuple[1], inUnionNameListPartSQL, smsTuple[2], a10, a20, smsTuple[3], a30))
+    for record in csvlist:
+        csvfile.write('|'.join(str(e) for e in record) + "\n")
+    csvfile.close()
 
 
-# dbConenectMessage = MySQLdb.connect(host='192.168.12.155', user='guoliufang', passwd='tiger2108', db='honeycomb',
-#                                     use_unicode=True, port=5209, charset='utf8'))
-# messageExecutor = dbConenectMessage.cursor()
-# sql = """select * from honeycomb.sms_received_histories_all_thread where content is not null and record_time between """ + start + """ and """ + end
-# messageExecutor.execute(sql)
-# messageContent = messageExecutor.fetchall()
 noProvince()
 os.system(
     """/usr/local/Calpont/bin/cpimport honeycomb jieguohuizong -s '|' /data/sdg/guoliufang/mysqloutfile/noProvince.txt""")
+withProvince()
+os.system(
+    """/usr/local/Calpont/bin/cpimport honeycomb jieguohuizong -s '|' /data/sdg/guoliufang/mysqloutfile/withProvince.txt""")
